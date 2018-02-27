526dc3d320f6a2f2c4cb6a39fd8b3b8c135f054c
alya <delirium.of.insane@gmail.com>
2018-02-27 09:41:34 +0300

    Drawer has been added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Divider from 'material-ui/Divider';
 
-class App extends Component {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  appFrame: {
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    width: `calc(100% - 320px)`,
+  },
+  'appBar-left': {
+    marginLeft: 320,
+  },
+  'appBar-right': {
+    marginRight: 320,
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: 320,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class PermanentDrawer extends React.Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-          <h1>Chat</h1>
-      </div>
+        <div className={classes.appFrame}>
+          <AppBar
+            position="absolute"
+            className={(classes.appBar)}
+          >
+            <Toolbar>
+              <Typography variant="title" color="inherit" noWrap>
+                DogeCodes React
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+                variant="permanent"
+                classes={{
+                paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.toolbar} />
+                <Divider />
+            </Drawer>
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Typography>{'You think water moves fast? You should see ice.'}</Typography>
+          </main>
+        </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(PermanentDrawer);
